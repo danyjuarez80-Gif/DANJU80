3ce0063fae1fe22be18330b153fb3d368a4ca810
danyjuarez80-Gif <danyjuarez80@gmail.com>
2026-05-14 12:53:37 -0500

    Delete update_list.sh

--- update_list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git pull origin main --rebase
-echo "Iniciando rastreo real..."
-python3 bot_userland.py
-echo "Subiendo cambios a GitHub..."
-git add .
-git commit -m "Update desde celular"
-git push origin main
